b2c0a41391afe4df79da28f3699b0f18dca3b954
wspjoy2011 <wspjoy2011@gmail.com>
2022-11-18 17:40:15 +0200

    add .gitignore

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import socket
 
 
 def start_web_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('127.0.0.1', 2000))
+    server.listen(5)
     try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(('127.0.0.1', 2000))
-        server.listen(5)
         while True:
             print('Server waiting...')
             client_socket, address = server.accept()
@@ -20,20 +20,25 @@ def start_web_server():
 
 
 def load_page_from_get_request(request):
-    HEADERS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-    HEADERS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-    path = request.split(' ')[1]
+    try:
+        path = request.split(' ')[1]
+    except IndexError:
+        path = '/'
+
     if path == '/':
         print(path)
         path = '/home.html'
-    response = ''
+
     try:
         with open('views' + path, 'rb') as file:
             response = file.read()
         return HEADERS.encode('utf-8') + response
     except FileNotFoundError:
-        return (HEADERS + 'Sorry, page not found...').encode('utf-8')
+        return (HEADERS_404 + 'Sorry, page not found...').encode('utf-8')
 
+
+HEADERS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+HEADERS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
 
 if __name__ == '__main__':
     start_web_server()
